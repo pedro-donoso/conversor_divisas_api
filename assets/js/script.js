92c5d261d7b4ce2eabb1abbaab3a7cbdd39f0ceb
pedro-donoso <pedro.donoso.r@protonmail.com>
2020-08-16 00:56:04 -0400

    defino EURO como divisa base

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 let inputs = document.querySelectorAll(".divisa_valor");
 
-let url = "https://api.exchangeratesapi.io/latest?base=USD";
+let url = "https://api.exchangeratesapi.io/latest?symbols=USD,GBP,JPY";
 
 //----fetch----
 fetch(url)
     .then(r => r.json())
     .then(data => {
-        document.querySelector("#EUR").dataset.cambio = data.rates.EUR;
+        document.querySelector("#USD").dataset.cambio = data.rates.USD;
         document.querySelector("#GBP").dataset.cambio = data.rates.GBP;
         document.querySelector("#JPY").dataset.cambio = data.rates.JPY;
+        
+        inputs.forEach(input => {
+            input.value = input.dataset.cambio;
+        });
+        
+
     })
     .catch(error => console.error(error))
 //----fetch----
@@ -17,7 +23,7 @@ function valorModificado(input) {
     let factor = input.value / input.dataset.cambio;
 
     inputs.forEach(input => {
-        input.value = (input.dataset.cambio * factor).toFixed(2);
+        input.value = (input.dataset.cambio * factor);
     });
 }
 

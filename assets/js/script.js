c1b2674dfa7396fb86b4a7107e7d97c15734e5a9
pedro-donoso <pedro.donoso.r@protonmail.com>
2020-08-16 00:20:04 -0400

    agrego peticiones con fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,26 @@
 let inputs = document.querySelectorAll(".divisa_valor");
-console.log(inputs);
 
-inputs.forEach(input => {
-    input.value = input.dataset.cambio;
-});
+let url = "https://api.exchangeratesapi.io/latest?base=USD";
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+
+    })
+    .then(function (data) {
+        console.log(data.rates);
+    })
+    .catch(function (error) {
+        console.error(error);
+    })
+
 
 function valorModificado(input) {
     let factor = input.value / input.dataset.cambio;
 
-        inputs.forEach(input => {
-            input.value = (input.dataset.cambio * factor).toFixed(2);
-        });
+    inputs.forEach(input => {
+        input.value = (input.dataset.cambio * factor).toFixed(2);
+    });
 
 
 

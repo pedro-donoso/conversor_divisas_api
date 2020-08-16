4879bccf670b176efdc1fc579f1b125d7c25e566
pedro-donoso <pedro.donoso.r@protonmail.com>
2020-08-16 00:43:44 -0400

    agrego ID divisas para asociarlas con javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,16 @@ let inputs = document.querySelectorAll(".divisa_valor");
 
 let url = "https://api.exchangeratesapi.io/latest?base=USD";
 
-/* fetch 
-
+//----fetch----
 fetch(url)
     .then(r => r.json())
-    .then(data => console.log(data.rates))
+    .then(data => {
+        document.querySelector("#EUR").dataset.cambio = data.rates.EUR;
+        document.querySelector("#GBP").dataset.cambio = data.rates.GBP;
+        document.querySelector("#JPY").dataset.cambio = data.rates.JPY;
+    })
     .catch(error => console.error(error))
-
-    fetch */
+//----fetch----
 
 function valorModificado(input) {
     let factor = input.value / input.dataset.cambio;
